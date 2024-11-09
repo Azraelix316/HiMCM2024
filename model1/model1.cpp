8fd2f1b5ba290c0e48380ce852f4365cb423d304
Azraelix316 <jaredj13310@gmail.com>
2024-11-09 12:49:09 +0800

    Adds file i/o

--- model1/model1.cpp
+++ model1/model1.cpp
@@ -9,4 +9,29 @@
 #include <random>
 using namespace std;
 int main() {
+    fstream fileInput;
+    //opens data file
+    fileInput.open("HiMCM_Olympic_Data (1).csv",ios::in);
+    vector<double> data;
+    //data collection
+    vector<string> curr;
+    string row,temp,line,col;
+while (fileInput >> temp) {
+        getline(fileInput, line);
+        stringstream row(line);
+    curr.clear();
+        // read every column data of a row and
+        // store it in a string variable, 'word'
+        while (getline(row, col, ','))
+        {
+            // add all the column data
+            // of a row to a vector
+            curr.push_back(col);
+        }
+        for (int i=0;i<curr.size();i++) {
+        cout << curr[i] <<" ";
+        }
+        cout << "\n";
+}
+
 }
