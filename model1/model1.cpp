2084fdca2d14ef31a9fc7c862c54992bcdecdb2e
Azraelix316 <jaredj13310@gmail.com>
2024-11-09 15:59:26 +0800

    Fixes File I/O, modifies data

--- model1/model1.cpp
+++ model1/model1.cpp
@@ -13,25 +13,30 @@ int main() {
     //opens data file
     fileInput.open("HiMCM_Olympic_Data (1).csv",ios::in);
     vector<double> data;
+    unordered_map<int,string> indexToSportMap;
     //data collection
     vector<string> curr;
     string row,temp,line,col;
+    cout << "\n\n";
 while (fileInput >> temp) {
-        getline(fileInput, line);
-        stringstream row(line);
+        if (temp.length()>0) {
     curr.clear();
         // read every column data of a row and
         // store it in a string variable, 'word'
-        while (getline(row, col, ','))
+        while (getline(fileInput, col, ','))
         {
             // add all the column data
             // of a row to a vector
             curr.push_back(col);
+            if (col.empty()) curr.push_back("0");
         }
+        //loops through and prints the data
+        //do you vector management stuff here
         for (int i=0;i<curr.size();i++) {
         cout << curr[i] <<" ";
         }
         cout << "\n";
+        }
 }
 
 }
