97fea68ab5a78c2e2789c096cbf31ead0fdacb56
Azraelix316 <jaredj13310@gmail.com>
2024-11-18 18:23:46 +0800

    Adds boolean flag, full working model

--- model1/model1.cpp
+++ model1/model1.cpp
@@ -4,10 +4,9 @@
 #include <fstream>
 #include <sstream>
 #include <cmath>
-#include <unordered_map>
 #include <algorithm>
-//#include <random>
-double trainingSpeed=0.1;
+#include <random>
+double trainingSpeed=1;
 using namespace std;
 bool is_number(string line)
 {
@@ -19,13 +18,11 @@ bool is_number(string line)
 double ssqr(vector<vector<double>> data, vector<double> coefficients) {
 double residuals=0;
 for (int i=0;i<data.size();i++) {
-    double results=0;
-    for (int j=0;j<coefficients.size();j++) {
-    results+=coefficients[j]*data[i][j];
-    }
-    residuals+=pow(results-data[i][data[i].size()-1],2);
+    double predicted=(data[i][5]/2028)*(data[i][0]*coefficients[0] + coefficients[1]*data[i][1] + coefficients[2]*data[i][2]+coefficients[3]*data[i][3]/5000000.0 + coefficients[4]*data[i][4]/2028+coefficients[5])+coefficients[6];
+    residuals+=pow(predicted-data[i][6],2);
 }
-cout << residuals << " ";
+//y=ax1 + bx2 + cx3
+//y=ax1x3 + bx2x3 
 return residuals;
 }
 
@@ -55,11 +52,17 @@ curr.push_back("0");
             curr.clear();
         }
 
-int yearsBeginColumn=5;
+int yearsBeginColumn=8;
+int dataBeginColumn=3;
 
 for (int i=1;i<data.size();i++) {
 if (data[i][0]=="0") {
 data[i][0]=data[i-1][0];
+}
+if (data[i][dataBeginColumn]!="-") {
+data[i][dataBeginColumn]="1";
+} else {
+data[i][dataBeginColumn]="0";
 }
 if (data[i][1]=="0") {
 data[i][1]=data[i][0];
@@ -72,9 +75,8 @@ data[i][j]="0";
 }
 
 vector<vector<double>> testcases;
-int dataBeginColumn=4;
 vector<string> years{"1896","1900","1904","1906","1908","1912","1920","1924","1928","1932","1936","1948","1952","1960","1964","1968","1972","1976","1980","1984","1988","1992","1996","2000","2004","2008","2012","2016","2020","2024","2028"};
-vector<double> coefficients{0,0};
+vector<double> coefficients{11.2298,1.47678,1.00034,-0.270256,-66.6671,94.4298,-40.4355};
 //ax1+bx2
 for (int row=1;row<data.size();row++) {
     for (int col=yearsBeginColumn;col<data[row].size()-1;col++) {
@@ -87,12 +89,44 @@ for (int row=1;row<data.size();row++) {
     }    
     testcases.push_back(dInput);
     }
-    }
+}
+// for (int i=0;i<testcases.size();i++) {
+// for (int j=0;j<testcases[i].size();j++) {
+// cout << testcases[i][j] << " ";
+// }
+// cout << "\n";
+// }
     double lastssqr;
-    for (int i=0;i<100;i++) {
+        bool improvement=true;
+while (trainingSpeed>0.00001) {
+    improvement=true;
+    while (improvement) {
+        improvement=false;
     for (int j=0;j<coefficients.size();j++) {
-    while (ssqr(testcases,coefficients)>ssqr(testcases,coefficients))
+        lastssqr=ssqr(testcases,coefficients);
+        coefficients[j]+=trainingSpeed;
+        while (lastssqr>ssqr(testcases,coefficients)) {
+        lastssqr=ssqr(testcases,coefficients);
+        coefficients[j]+=trainingSpeed;
+        improvement=true;
+        }
+        coefficients[j]-=trainingSpeed;
+        lastssqr=ssqr(testcases,coefficients);  
+        coefficients[j]-=trainingSpeed; 
+        while (lastssqr>ssqr(testcases,coefficients)) {
+        lastssqr=ssqr(testcases,coefficients);
+        coefficients[j]-=trainingSpeed;
+        improvement=true;
+        }
+        coefficients[j]+=trainingSpeed;
+        trainingSpeed+=trainingSpeed*0.01;
     }  
-    }  
-    cout << ssqr(testcases,coefficients);
+    cout << "\n" << ssqr(testcases,coefficients) << "\n";
+    for (int i=0;i<coefficients.size();i++) {
+    cout << coefficients[i]<< ",";
+    } 
+    } 
+    trainingSpeed=trainingSpeed/2.0;
+    cout << "\n"<< "Training Speed:" << trainingSpeed << "\n";
+}
 }
